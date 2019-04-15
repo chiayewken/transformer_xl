62b815fc82f5727d241e3d616bb083b27982cd00
Chia Yew Ken <chiayewken@gmail.com>
2019-04-15 23:19:28 +0800

    integrate bpe from gpt-2 implementation

---
+++ tf/data_utils_bpe.py
@@ -0,0 +1,140 @@
+from __future__ import absolute_import, division, print_function
+
+import json
+import os
+import pickle
+
+import tensorflow
+from absl import flags
+
+from tf.vocabulary_bpe import get_encoder
+from tf.data_utils import Corpus
+
+
+class BPECorpus(Corpus):
+    def __init__(self, path, dataset, *args, **kwargs):
+        self.dataset = dataset
+        self.vocab = get_encoder()
+
+        if self.dataset == "bookcorpus":
+            # assumes train.txt has been sharded to avoid OOM eg train00, train01 etc
+            train_path_pattern = os.path.join(path, "train*")
+            self.train = tensorflow.gfile.Glob(train_path_pattern)
+            self.valid = self.vocab.encode_file(os.path.join(path, "valid.txt"))
+            self.test = self.vocab.encode_file(os.path.join(path, "test.txt"))
+
+        elif self.dataset == "lm1b":
+            train_path_pattern = os.path.join(
+                path,
+                "1-billion-word-language-modeling-benchmark-r13output",
+                "training-monolingual.tokenized.shuffled",
+                "news.en-*",
+            )
+            self.train = tensorflow.gfile.Glob(train_path_pattern)
+            self.valid = self.vocab.encode_file(os.path.join(path, "valid.txt"))
+            self.test = self.vocab.encode_file(os.path.join(path, "valid.txt"))
+
+        else:
+            self.train = self.vocab.encode_file(os.path.join(path, "train.txt"))
+            self.valid = self.vocab.encode_file(os.path.join(path, "valid.txt"))
+            self.test = self.vocab.encode_file(os.path.join(path, "test.txt"))
+
+        # not sure what this is for
+        self.cutoffs = []
+
+
+def get_lm_corpus(data_dir, dataset):
+    fn = os.path.join(data_dir, "cache.pkl")
+
+    if tensorflow.gfile.Exists(fn):
+        print("Loading cached dataset...")
+        with open(fn, "rb") as fp:
+            corpus = pickle.load(fp)
+    else:
+        print("Producing dataset...")
+        corpus = BPECorpus(data_dir, dataset)
+
+        print("Saving dataset...")
+        with open(fn, "wb") as fp:
+            pickle.dump(corpus, fp, protocol=2)
+
+        corpus_info = {
+            "vocab_size": len(corpus.vocab),
+            "cutoffs": corpus.cutoffs,
+            "dataset": corpus.dataset,
+        }
+        with open(os.path.join(data_dir, "corpus-info.json"), "w") as fp:
+            json.dump(corpus_info, fp)
+
+    return corpus
+
+
+def main(unused_argv):
+    del unused_argv  # Unused
+
+    corpus = get_lm_corpus(FLAGS.data_dir, FLAGS.dataset)
+
+    save_dir = os.path.join(FLAGS.data_dir, "tfrecords")
+    if not tensorflow.gfile.Exists(save_dir):
+        tensorflow.gfile.MakeDirs(save_dir)
+
+    # test mode
+    if FLAGS.per_host_test_bsz > 0:
+        corpus.convert_to_tfrecords(
+            "test",
+            save_dir,
+            FLAGS.per_host_test_bsz,
+            FLAGS.tgt_len,
+            FLAGS.num_core_per_host,
+            FLAGS=FLAGS,
+        )
+        return
+
+    for split, batch_size in zip(
+        ["train", "valid"], [FLAGS.per_host_train_bsz, FLAGS.per_host_valid_bsz]
+    ):
+
+        if batch_size <= 0:
+            continue
+        print("Converting {} set...".format(split))
+        corpus.convert_to_tfrecords(
+            split,
+            save_dir,
+            batch_size,
+            FLAGS.tgt_len,
+            FLAGS.num_core_per_host,
+            FLAGS=FLAGS,
+        )
+
+
+if __name__ == "__main__":
+    FLAGS = flags.FLAGS
+    flags.DEFINE_string("data_dir", None, help="Location of the data corpus")
+    flags.DEFINE_enum(
+        "dataset",
+        "wt103",
+        ["ptb", "wt2", "wt103", "lm1b", "enwik8", "text8", "bookcorpus"],
+        help="Dataset name.",
+    )
+    flags.DEFINE_integer("per_host_train_bsz", 60, help="train batch size each host")
+    flags.DEFINE_integer("per_host_valid_bsz", 60, help="valid batch size each host")
+    flags.DEFINE_integer(
+        "per_host_test_bsz",
+        0,
+        help="If > 0, enter test mode and process test set only."
+        "Otherwise, process train and dev sets only.",
+    )
+    flags.DEFINE_integer("tgt_len", 70, help="number of tokens to predict")
+    flags.DEFINE_integer("max_batch", -1, help="run in debug mode")
+    flags.DEFINE_integer("num_core_per_host", 8, help="8 for TPU v2.")
+    flags.DEFINE_bool(
+        "debug",
+        default=False,
+        help="Process only the first batch without shuffle for lm1b.",
+    )
+    flags.DEFINE_integer("num_procs", 1, help="number of processes")
+    flags.DEFINE_integer("num_passes", 10, help="number of passes when use_tpu=True")
+    flags.DEFINE_integer("num_shuffle", 4, help="number of shuffles for lm1b")
+    flags.DEFINE_bool("use_tpu", True, help="use tpu")
+
+    tensorflow.app.run(main)
---
+++ tf/scripts/wt103_large_tpu_bpe.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+echo mode: $1
+echo gsutil dir: $2
+
+# num_passes repeats data
+# default is 10 to mitigate TPU dropping remainder on small datasets
+# should be able to set to 1 on large datasets
+echo num training passes: $3
+
+# Path
+# trailing "/" uncecessary
+LOCAL_DIR=../data/wikitext-103
+GSDATA=$2
+GSEXP=$2
+
+# TPU setting
+NUM_HOST=1 # Colab TPUv2 -> 1 | Depends on TPU configuration eg pods have more
+NUM_CORE=8 # TPUv2 -> 8 | TPUv3 -> 16
+
+TEST_NUM_HOST=1
+TEST_NUM_CORE=8 # TPUv2 -> 8 | TPUv3 -> 16
+
+# Model
+DIV_VAL=4
+N_LAYER=18
+D_MODEL=1024
+D_EMBED=1024
+N_HEAD=16
+D_HEAD=64
+D_INNER=4096
+
+# Training
+TGT_LEN=384
+MEM_LEN=384
+# TRAIN_BSZ=128
+# VALID_BSZ=128
+# BSZ 128 + Colab TPU ->  Used 18.76G of 8.00G hbm
+# BSZ 32 + Colab TPU ->  Used 8.41G of 8.00G hbm
+TRAIN_BSZ=24  
+VALID_BSZ=24
+
+# Testing
+TEST_TGT_LEN=128
+TEST_MEM_LEN=1600
+TEST_CLAMP_LEN=1000
+TEST_BSZ=8
+
+if [[ $1 == 'train_data' ]]; then
+    # python data_utils.py \
+    python data_utils_bpe.py \
+        --data_dir=${LOCAL_DIR}/ \
+        --dataset=wt103 \
+        --tgt_len=${TGT_LEN} \
+        --per_host_train_bsz=${TRAIN_BSZ} \
+        --per_host_valid_bsz=${VALID_BSZ} \
+        --num_core_per_host=${NUM_CORE} \
+        --num_passes=$3 \
+        --use_tpu=True \
+        ${@:2}
+
+    # SRC_PATTERN=train.bsz-${TRAIN_BSZ}.tlen-${TGT_LEN}.core-${NUM_CORE}*
+    # gsutil -m cp ${LOCAL_DIR}/tfrecords/${SRC_PATTERN} ${GSDATA}/bookcorpus-tfrecords/
+    # SRC_PATTERN=valid.bsz-${VALID_BSZ}.tlen-${TGT_LEN}.core-${NUM_CORE}*
+    # gsutil -m cp ${LOCAL_DIR}/tfrecords/${SRC_PATTERN} ${GSDATA}/bookcorpus-tfrecords/
+
+    # include corpus metadata in gcloud bucket
+    gsutil -m cp ${LOCAL_DIR}/tfrecords/* ${GSDATA}/bookcorpus-tfrecords/
+    gsutil -m cp ${LOCAL_DIR}/cache.pkl ${GSDATA}/bookcorpus-tfrecords/
+    gsutil -m cp ${LOCAL_DIR}/corpus-info.json ${GSDATA}/bookcorpus-tfrecords/
+
+elif [[ $1 == 'test_data' ]]; then
+    # python data_utils.py \
+    python data_utils_bpe.py \
+        --data_dir=${LOCAL_DIR}/ \
+        --dataset=wt103 \
+        --tgt_len=${TEST_TGT_LEN} \
+        --per_host_test_bsz=${TEST_BSZ} \
+        --num_core_per_host=${TEST_NUM_CORE} \
+        --num_passes=1 \
+        --use_tpu=True \
+        ${@:2}
+
+    SRC_PATTERN=test.bsz-${TEST_BSZ}.tlen-${TEST_TGT_LEN}.core-${TEST_NUM_CORE}*
+    gsutil -m cp ${LOCAL_DIR}/tfrecords/${SRC_PATTERN} ${GSDATA}/wt103-tfrecords/
+
+elif [[ $1 == 'train' ]]; then
+    echo 'Run training...'
+    python train.py \
+        --data_dir=${GSDATA}/wt103-tfrecords \
+        --record_info_dir=${LOCAL_DIR}/tfrecords/ \
+        --corpus_info_path=${LOCAL_DIR}/corpus-info.json \
+        --model_dir=${GSEXP}/wt103 \
+        --div_val=${DIV_VAL} \
+        --untie_r=True \
+        --proj_share_all_but_first=True \
+        --proj_same_dim=True \
+        --n_layer=${N_LAYER} \
+        --d_model=${D_MODEL} \
+        --d_embed=${D_EMBED} \
+        --n_head=${N_HEAD} \
+        --d_head=${D_HEAD} \
+        --d_inner=${D_INNER} \
+        --dropout=0.2 \
+        --dropatt=0.2 \
+        --init_std=0.005 \
+        --learning_rate=0.00025 \
+        --warmup_steps=16000 \
+        --train_steps=4000000 \
+        --tgt_len=${TGT_LEN} \
+        --mem_len=${MEM_LEN} \
+        --train_batch_size=${TRAIN_BSZ} \
+        --num_hosts=${NUM_HOST} \
+        --num_core_per_host=${NUM_CORE} \
+        --iterations=1000 \
+        --save_steps=10000 \
+        --use_tpu=True \
+        --do_eval=False \
+        ${@:2}
+
+elif [[ $1 == 'eval' ]]; then
+    echo 'Run evaluation...'
+    python train.py \
+        --data_dir=${GSDATA}/wt103-tfrecords \
+        --record_info_dir=${LOCAL_DIR}/tfrecords/ \
+        --corpus_info_path=${LOCAL_DIR}/corpus-info.json \
+        --model_dir=${GSEXP}/wt103 \
+        --div_val=${DIV_VAL} \
+        --untie_r=True \
+        --proj_share_all_but_first=True \
+        --proj_same_dim=True \
+        --n_layer=${N_LAYER} \
+        --d_model=${D_MODEL} \
+        --d_embed=${D_EMBED} \
+        --n_head=${N_HEAD} \
+        --d_head=${D_HEAD} \
+        --d_inner=${D_INNER} \
+        --tgt_len=${TEST_TGT_LEN} \
+        --mem_len=${TEST_MEM_LEN} \
+        --clamp_len=${TEST_CLAMP_LEN} \
+        --same_length=True \
+        --eval_batch_size=${TEST_BSZ} \
+        --num_host=${TEST_NUM_HOST} \
+        --num_core_per_host=${TEST_NUM_CORE} \
+        --use_tpu=True \
+        --do_train=False \
+        --do_eval_only=True \
+        --eval_split=test \
+        ${@:2}
+
+else
+    echo 'unknown argment 1'
+fi
